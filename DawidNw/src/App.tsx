8dc35e90983e068a4b67db9c077bf9572e08aa11
Dawid <31769799+wybielak@users.noreply.github.com>
2024-04-17 18:01:24 +0200

    useState i useRef

--- DawidNw/src/App.tsx
+++ DawidNw/src/App.tsx
@@ -1,13 +1,44 @@
-function lowerLetters(text: string) {
-  return text.toLowerCase()
+import { useState } from "react"
+import TaskList from "./components/TaskList"
+import AddNewTask from "./components/AddNewTask"
+
+export type TaskT = {
+  title: string,
+  author: string,
+  deadline: string,
 }
 
+const mockTasks: TaskT[] = [
+  {
+    title: "Task 1",
+    author: "Dawid",
+    deadline: "18-04-2024"
+  },
+  {
+    title: "Task 2",
+    author: "Dawid",
+    deadline: "19-04-2024"
+  },
+  {
+    title: "Task 3",
+    author: "Dawid",
+    deadline: "20-04-2024"
+  },
+]
 
 function App() {
 
+  const [tasks, setTasks] = useState(mockTasks) 
+
   return (
     <>
-      <h1>{lowerLetters("Hello World!")}</h1>
+      <main className="p-4">
+        <section className="flex flex-col gap-8">
+          <h1 className="text-4xl font-bold">My tasks</h1>
+          <AddNewTask setTasks={setTasks}/>
+          <TaskList tasks={tasks} />
+        </section>
+      </main>
     </>
   )
 }
