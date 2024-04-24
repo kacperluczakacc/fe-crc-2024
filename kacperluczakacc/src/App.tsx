76c1000e3aebd25b5f8f1d055161e75259a2e633
SE86082 <kacper.luczak2@external.stellantis.com>
2024-04-24 18:00:46 +0200

    Add all

--- kacperluczakacc/src/App.tsx
+++ kacperluczakacc/src/App.tsx
@@ -4,8 +4,13 @@
 
 import { useState } from "react";
 import Task from "./components/Task";
-import TaskList from "./components/TaskList";
-import AddTask from "./components/AddTask";
+
+import { AddTask, TaskList } from './pages';
+
+import { Route, Switch } from "react-router-dom";
+import { ROUTE } from "./lib/constants";
+import Header from "./components/Header";
+import { sortByNewest } from "./lib/helpers/dates";
 
 export type Task = {
     title: string;
@@ -39,13 +44,17 @@ const App = () => {
     const [tasks, setTasks] = useState(mockTasks);
 
     return (
-        <main className="p-4">
-            <section className="flex flex-col gap-8">
-                <h1 className="text-4xl font-bold">My tasks</h1>
-                <AddTask setTasks={setTasks} />
-                <TaskList tasks={tasks} />
-            </section>
-        </main>
+        <>  
+            <Header />
+            <Switch>
+                <Route path={ROUTE.ADD_TASK}>
+                    <AddTask setTasks={setTasks} />
+                </Route>
+                <Route path={ROUTE.HOME}>
+                    <TaskList tasks={tasks} />
+                </Route>
+            </Switch>
+        </>
     )
 }
 
