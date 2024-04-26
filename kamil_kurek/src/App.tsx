9e8060203503681d02da36694338a99e79749c9f
BigMop13 <kamil.kurek1212@gmail.com>
2024-04-26 14:24:29 +0200

    checkboxes, routing

--- kamil_kurek/src/App.tsx
+++ kamil_kurek/src/App.tsx
@@ -1,47 +1,61 @@
-//komponenty w react są funkcjami, które zwracają jeden element DOM
+// Komponenty w React.js są funkcjami, które zwracają JEDEN element DOM.
+// Składnią React.js jest JSX.
+// Argumenty przekazywane do komponentów Reactowych nazywamy PROPS
 
-import {useState} from "react";
-import TaskList from "./components/TaskList.tsx";
-import AddTask from "./components/AddTask.tsx";
+import { useState } from "react";
+import Task from "./components/Task";
 
-export type TaskType = {
+import { AddTask, TaskList } from './pages';
+
+import { Route, Switch } from "react-router-dom";
+import { ROUTE } from "./lib/constants";
+import Header from "./components/Header";
+import { sortByNewest } from "./lib/helpers/dates";
+
+export type Task = {
   title: string;
   author: string;
   deadline: string;
 }
 
-const mockTasks: TaskType[] = [
+// "Coming from server"
+const mockTasks: Task[] = [
   {
     title: "Task 1",
-    author: "Kamil",
+    author: "Kacper",
     deadline: "18/09/2025"
   },
   {
     title: "Task 2",
-    author: "Kamil",
-    deadline: "25/09/2025"
+    author: "Tomek",
+    deadline: "18/09/2024"
   },
   {
     title: "Task 3",
-    author: "Kamil",
+    author: "Gosia",
     deadline: "18/01/2025"
-  },
-]
+  }
+];
 
-//hooks to wbudowane funkcje/narzędzia pozwalająca na manipulowanie różnymi elementami/cyklami komponentów
+// Hooks to wbudowane funkcje/narzędzia, które pozwalają nam w prosty sposób manipulować
+// różnymi elementami/cyklami komponentów w React.js
 
 const App = () => {
-  const [tasks, setTasks] = useState(mockTasks)
+  const [tasks, setTasks] = useState(mockTasks);
 
   return (
-      <main className="p-4">
-        <section className="flex flex-col gap-8">
-          <h1 className="text-4xl font-bold">My tasks</h1>
-          <AddTask setTasks={setTasks}/>
-          <TaskList tasks={tasks} />
-        </section>
-      </main>
-  );
+      <>
+        <Header />
+        <Switch>
+          <Route path={ROUTE.ADD_TASK}>
+            <AddTask setTasks={setTasks} />
+          </Route>
+          <Route path={ROUTE.HOME}>
+            <TaskList tasks={tasks} />
+          </Route>
+        </Switch>
+      </>
+  )
 }
 
 export default App
