ecc623c2c22f6f27bbd05dcb7e8049e622e5dca3
Jakub Sikora <sikorajakub@proton.me>
2024-04-26 09:53:45 +0200

    Module 4 homework done - WITH QUESTION IN COMMENTS

--- sikortm/src/pages/AddTask.tsx
+++ sikortm/src/pages/AddTask.tsx
@@ -1,6 +1,6 @@
 import { MdClose as CloseIcon } from 'react-icons/md'
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Task } from "../App"
 import { RegExp } from '../lib/constans';
 
@@ -10,35 +10,54 @@ type AddTaskProps = {
 
 export default function AddTask({setTasks}: AddTaskProps) {
     const [taskName, setTaskName] = useState('');
+    const [author, setAuthor] = useState('');
     const [deadline, setDeadline] = useState('');
 
     const [taskNameError, setTaskNameError] = useState(false);
+    const [authorError, setAuthorError] = useState(false);
     const [deadlineError, setDeadlineError] = useState(false);
 
-    // Zrobić walidację tekstu dla Author (przerobić na reużywalną funkcję)
-
-    function hideTaskError() {
-        setTaskNameError(false);
-    }
-    function hideDeadlineError() {
-        setDeadlineError(false);
+    function hideError(errorFunc : React.Dispatch<React.SetStateAction<boolean>>) {
+        errorFunc(false);
     }
 
-    function handleAddTask() {
-        if (taskName.length == 0) {
-            setTaskNameError(true);
+    function validateTextInput(value : string, errorFunc : React.Dispatch<React.SetStateAction<boolean>>) : boolean {
+        if (value.trim().length == 0) {
+            errorFunc(true);
+            return true;
         }
-        if (!RegExp.deadline.test(deadline)) {
-            setDeadlineError(true);
+        return false;
+    }
+
+    function validateDateInput(value : string, errorFunc : React.Dispatch<React.SetStateAction<boolean>>) : boolean {
+        if (!RegExp.deadline.test(value)) {
+            errorFunc(true);
+            return true;
         }
-        // setTasks(prevTasks => [
-        //     ...prevTasks,
-        //     {
-        //         title: taskNameRef.current!.value,
-        //         title: authorRef.current!.value,
-        //         title: deadlineRef.current!.value
-        //     }
-        // ]);
+        return false;
+    }
+
+    async function handleAddTask() {
+        const taskNameTrigger : boolean = validateTextInput(taskName, setTaskNameError); 
+        const authorTrigger : boolean = validateTextInput(author, setAuthorError);
+        const deadlineTrigger : boolean = validateDateInput(deadline, setDeadlineError);
+
+        // Chciałem tutaj użyć taskNameError, authorError i deadlineError,
+        // ale stan w Reacie aktualizuje się najwidoczniej asynchronicznie,
+        // więc muszę to zrobić przypisując zwracaną wartość do stałych.
+        // Od razu tu moje pytanie czy można to rozwiązać inaczej tak, aby
+        // nie tworzyć dodatkowych zmiennych/stałych?
+        if (taskNameTrigger || authorTrigger || deadlineTrigger)
+            return;
+
+        setTasks(prevTasks => [
+            ...prevTasks,
+            {
+                title: taskName,
+                author: author,
+                deadline: deadline
+            }
+        ]);
     }
 
     return (
@@ -52,18 +71,19 @@ export default function AddTask({setTasks}: AddTaskProps) {
 
                 <div className='flex flex-col relative'>
                     <label className='absolute -top-3 left-2 bg-secondary px-2'>Task Name</label>
-                    <input onFocus={hideTaskError} onInput={inputValue => setTaskName(inputValue.currentTarget.value)} className="border h-14 p-4" type="text" />
+                    <input onFocus={() => hideError(setTaskNameError)} onInput={inputValue => setTaskName(inputValue.currentTarget.value.trim())} className="border h-14 p-4" type="text" />
                     { taskNameError && <p>Task name is empty. Please add text</p> }
                 </div>
 
                 <div className='flex flex-col relative'>
                     <label className='absolute -top-3 left-2 bg-secondary px-2'>Author</label>
-                    <input className="border h-14 p-4" type="text" />
+                    <input onFocus={() => hideError(setAuthorError)} onInput={inputValue => setAuthor(inputValue.currentTarget.value.trim())} className="border h-14 p-4" type="text" />
+                    { authorError && <p>Author is empty. Please add text</p> }
                 </div>
 
                 <div className='flex flex-col relative'>
                     <label className='absolute -top-3 left-2 bg-secondary px-2'>Deadline</label>
-                    <input onFocus={hideDeadlineError} onInput={inputValue => setDeadline(inputValue.currentTarget.value)} className="border h-14 p-4" type="text" />
+                    <input onFocus={() => hideError(setDeadlineError)} onInput={inputValue => setDeadline(inputValue.currentTarget.value)} className="border h-14 p-4" type="text" />
                     { deadlineError && <p>Deadline is in a wrong format (DD/MM/YYYY). Please correct</p> }
                 </div>
 
