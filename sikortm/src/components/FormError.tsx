c1892b1ffb49f2d334d9e8ce3e1a529dd7098284
SikorTM <sikorajakub@proton.me>
2024-04-29 20:59:45 +0200

    Changes after meeting, gave up on useReducer...

--- sikortm/src/components/FormError.tsx
+++ sikortm/src/components/FormError.tsx
@@ -1,7 +1,15 @@
-type FormError = {
-    title: string
-}
+export default function FormError({type, errorType}: {
+    type: 'title' | 'author' | 'deadline',
+    errorType: 'empty' | 'too-short' | 'wrong-format' | ''
+    }){
 
-export default function FormError({title}: FormError){
-    return <p className="text-red-700">{title}</p>;
+    return (
+        <p className='text-red-700'>{
+            type[0].toUpperCase() + type.slice(1) + (
+                errorType == 'empty' ? ' is required' :
+                (errorType == 'too-short' ? ' should be at least 3 characters' :
+                ' should be of DD/MM/YYYY format')
+            )
+        }</p>
+    );
 }
--- sikortm/src/pages/AddTask.tsx
+++ sikortm/src/pages/AddTask.tsx
@@ -3,12 +3,16 @@ import { MdClose as CloseIcon } from 'react-icons/md'
 import { useState } from "react";
 import { Task } from "../App"
 import { RegExp, ROUTE } from '../lib/constans';
-import { FormError } from '../components';
+import { FormLabeledInput } from '../components';
 import { Link, useHistory } from "react-router-dom";
 
 type AddTaskProps = {
     setTasks: React.Dispatch<React.SetStateAction<Task[]>>
 }
+
+export type FormInputType = 'title' | 'author' | 'deadline';
+
+export type FormErrorType = 'empty' | 'too-short' | 'wrong-format' | '';
 
 export default function AddTask({setTasks}: AddTaskProps) {
     const routerHistory = useHistory();
@@ -17,17 +21,11 @@ export default function AddTask({setTasks}: AddTaskProps) {
     const [author, setAuthor] = useState('');
     const [deadline, setDeadline] = useState('');
 
-    const [taskNameError, setTaskNameError] = useState('');
-    const [authorError, setAuthorError] = useState('');
-    const [deadlineError, setDeadlineError] = useState('');
+    const [taskNameError, setTaskNameError] = useState<FormErrorType>('');
+    const [authorError, setAuthorError] = useState<FormErrorType>('');
+    const [deadlineError, setDeadlineError] = useState<FormErrorType>('');
 
-    // Funkcja raczej nadająca się do helpera
-
-    function hideError(errorFunc : React.Dispatch<React.SetStateAction<string>>) {
-        errorFunc('');
-    }
-
-    function validateTextInput(value : string, errorFunc : React.Dispatch<React.SetStateAction<string>>) : boolean {
+    function validateTextInput(value : string, errorFunc : React.Dispatch<React.SetStateAction<FormErrorType>>) : boolean {
         if (value.trim().length == 0) {
             errorFunc('empty');
             return true;
@@ -39,7 +37,7 @@ export default function AddTask({setTasks}: AddTaskProps) {
         return false;
     }
 
-    function validateDateInput(value : string, errorFunc : React.Dispatch<React.SetStateAction<string>>) : boolean {
+    function validateDateInput(value : string, errorFunc : React.Dispatch<React.SetStateAction<FormErrorType>>) : boolean {
         if (value.trim().length == 0) {
             errorFunc('empty');
             return true;
@@ -52,9 +50,9 @@ export default function AddTask({setTasks}: AddTaskProps) {
     }
 
     function handleAddTask() {
-        const taskNameTrigger : boolean = validateTextInput(taskName, setTaskNameError); 
-        const authorTrigger : boolean = validateTextInput(author, setAuthorError);
-        const deadlineTrigger : boolean = validateDateInput(deadline, setDeadlineError);
+        const taskNameTrigger = validateTextInput(taskName, setTaskNameError); 
+        const authorTrigger = validateTextInput(author, setAuthorError);
+        const deadlineTrigger = validateDateInput(deadline, setDeadlineError);
 
         // Chciałem tutaj użyć taskNameError, authorError i deadlineError,
         // ale stan w Reacie aktualizuje się najwidoczniej asynchronicznie,
@@ -84,30 +82,29 @@ export default function AddTask({setTasks}: AddTaskProps) {
             </div>
             <form className="flex flex-col gap-10 my-4 px-5">
 
-                <div className='flex flex-col relative'>
-                    <label className='absolute -top-3 left-2 bg-secondary px-2'>Task Name</label>
-                    {
-                        // Ciekawe jest również to, że zastosowanie
-                        // taskNameError != '' && ' border-red-700'
-                        // powoduje, że nie wczytuje mi się p-4 i muszę zrobić albo
-                        // taskNameError != '' ? ' border-red-700' : ''
-                        // albo dodać spację na końcu dotyczasowego className
-                    }
-                    <input onFocus={() => hideError(setTaskNameError)} onInput={inputValue => setTaskName(inputValue.currentTarget.value.trim())} className={'border h-14 p-4 ' + (taskNameError != '' && 'border-red-700')} type="text" />
-                    { taskNameError == 'empty' ? <FormError title={'Title is required'}/> : taskNameError == 'too-short' && <FormError title={'Title should be at least 3 characters'}/> }
-                </div>
+                <FormLabeledInput
+                    title={'Title'}
+                    type={'title'}
+                    errorType={taskNameError}
+                    setError={setTaskNameError}
+                    setValue={setTaskName}
+                />
 
-                <div className='flex flex-col relative'>
-                    <label className='absolute -top-3 left-2 bg-secondary px-2'>Author</label>
-                    <input onFocus={() => hideError(setAuthorError)} onInput={inputValue => setAuthor(inputValue.currentTarget.value.trim())} className={'border h-14 p-4 ' + (taskNameError != '' && 'border-red-700')} type="text" />
-                    { authorError == 'empty' ? <FormError title={'Author is required'}/> : authorError == 'too-short' && <FormError title={'Author should be at least 3 characters'}/> }
-                </div>
+                <FormLabeledInput
+                    title={'Author'}
+                    type={'author'}
+                    errorType={authorError}
+                    setError={setAuthorError}
+                    setValue={setAuthor}
+                />
 
-                <div className='flex flex-col relative'>
-                    <label className='absolute -top-3 left-2 bg-secondary px-2'>Deadline</label>
-                    <input onFocus={() => hideError(setDeadlineError)} onInput={inputValue => setDeadline(inputValue.currentTarget.value)} className={'border h-14 p-4 ' + (taskNameError != '' && 'border-red-700')} type="text" />
-                    { deadlineError == 'empty' ? <FormError title={'Deadline is required'}/> : deadlineError == 'wrong-format' && <FormError title={'Deadline should be of DD/MM/YYYY format'} /> }
-                </div>
+                <FormLabeledInput
+                    title={'Deadline'}
+                    type={'deadline'}
+                    errorType={deadlineError}
+                    setError={setDeadlineError}
+                    setValue={setDeadline}
+                />
 
             </form>
         </section>
