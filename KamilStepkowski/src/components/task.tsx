ee268cebbd164f92b994dd14b29543b29ab812ce
Kamil Stępkowski <kamilst18@gmail.com>
2024-05-19 13:31:20 +0200

    New features.

--- KamilStepkowski/src/components/task.tsx
+++ KamilStepkowski/src/components/task.tsx
@@ -1,13 +1,26 @@
+import {
+  MdOutlineCheckBoxOutlineBlank as EmptyCheckbox,
+  MdOutlineCheckBox as CheckedCheckbox,
+} from "react-icons/md";
+
 import { Task as TaskType } from "../App";
+import { useState } from "react";
 
-function Task({title, author, deadline}: TaskType) {
+export default function Task({ title, author, deadline }: TaskType) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    function toggleCheck() {
+        setIsChecked(prevState => !prevState);
+    }
+
   return (
-    <div>
-      <p className="text-xl">{title}</p>
-      <p className="font-medium">{author}</p>
-      <p>{deadline}</p>
+    <div onClick={toggleCheck} className="bg-secondary rounded-2xl shadow-md active:bg-primary my-4 p-4 flex justify-between items-center">
+      <div>
+        <p>{deadline}</p>
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-slate-600">{author}</p>
+      </div>
+      {isChecked ? <CheckedCheckbox size={24} /> : <EmptyCheckbox size={24} />}
     </div>
-  )
+  );
 }
-
-export default Task;
