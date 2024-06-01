89912b8861612dbd1ee4bec34aab470253b297c0
Oskarowski <oscar.kacprzak@gmail.com>
2024-06-01 15:47:52 +0000

    rm not redundant code

--- Oskarowski/src/App.tsx
+++ Oskarowski/src/App.tsx
@@ -7,32 +7,6 @@ import { Route, Switch, useHistory } from "react-router-dom";
 import { ROUTE } from "./lib/routes.ts";
 import { useEffect } from "react";
 
-async function getTasks() {
-    const response = await fetch("http://localhost:3000/tasks");
-    const data = await response.json();
-    console.log(data);
-}
-
-async function addNewTask() {
-    const response = await fetch("http://localhost:3000/tasks", {
-        method: "POST",
-        body: JSON.stringify({
-            title: "New task no. 4",
-            author: "Kacper",
-            deadline: "19/09/2025",
-        }),
-    });
-}
-
-async function updateTask(id: string) {
-    const response = await fetch(`http://localhost:3000/tasks/${id}`, {
-        method: "PUT", // aktualizacja danych dla elementu w bazie danych reprezentowanych przed podany ID
-        body: JSON.stringify({
-            title: "Updated task no. 4",
-        }),
-    });
-}
-
 function App() {
     const history = useHistory();
 
